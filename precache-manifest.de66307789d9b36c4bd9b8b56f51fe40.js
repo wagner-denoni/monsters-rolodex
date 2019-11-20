2b6f343968d956cf44245ce22bad3a0d7a71d7d6
Wagner Freitas <wagnerdenoni@gmail.com>
2019-11-20 06:57:04 -0300

    Updates

---
+++ precache-manifest.de66307789d9b36c4bd9b8b56f51fe40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ec3cf634911162419c9292ba2566e7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2c0b6e064ae8a66ff5d",
+    "url": "/monsters-rolodex/static/css/main.83f2dc9c.chunk.css"
+  },
+  {
+    "revision": "15f19995be12077ccfb5",
+    "url": "/monsters-rolodex/static/js/2.1cc5d982.chunk.js"
+  },
+  {
+    "revision": "c2c0b6e064ae8a66ff5d",
+    "url": "/monsters-rolodex/static/js/main.2fe46864.chunk.js"
+  },
+  {
+    "revision": "998a932ffc96f80a64f5",
+    "url": "/monsters-rolodex/static/js/runtime-main.61a4f28e.js"
+  }
+]);
